4866d90a60d0db53f4b355bc0df1395d828dddd9
Shalama <shalamafadillaa@gmail.com>
2026-01-12 14:54:28 +0700

    Make the function return 'Hello'

--- src/capitalize.spec.js
+++ src/capitalize.spec.js
@@ -3,3 +3,7 @@ import { capitalize } from "./capitalize.js"
 test('capitalize is defined', () => {
     expect(capitalize).toBeDefined();
 })
+
+test('return Hello', () => {
+    expect(capitalize('hello')).toBe('Hello');
+})
