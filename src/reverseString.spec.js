50fc3e330349e4219f8f42bb78a19c437b54cc3e
Shalama <shalamafadillaa@gmail.com>
2026-01-12 15:54:51 +0700

    Make the function return 'olleh'
    Return 'olleh' from 'hello' parameter.

--- src/reverseString.spec.js
+++ src/reverseString.spec.js
@@ -3,3 +3,7 @@ import { reverseString } from "./reverseString.js";
 test('reverseString is defined', () => {
     expect(reverseString).toBeDefined();
 })
+
+test('Return olleh from hello', () => {
+    expect(reverseString('hello')).toBe('olleh');
+})
