5fcb1c220370dcb6717c4fca1779cee88e5abede
Shalama <shalamafadillaa@gmail.com>
2026-01-13 10:44:31 +0700

    Group tests using describe blocks

--- src/calculator.spec.js
+++ src/calculator.spec.js
@@ -5,89 +5,93 @@ test('calculator object is defined', () => {
 })
 
 
-// Tests for add function
-test('calculator object contains add function', () => {
-    expect(typeof calculator.add).toBe('function');
-})
+describe('add function', () => {
+    test('calculator object contains add function', () => {
+        expect(typeof calculator.add).toBe('function');
+    })
 
-test('make 2 + 2 return 4', () => {
-    expect(calculator.add(2, 2)).toBe(4);
-})
+    test('make 2 + 2 return 4', () => {
+        expect(calculator.add(2, 2)).toBe(4);
+    })
 
-test('make 123 + 200 return 6', () => {
-    expect(calculator.add(123, 200)).toBe(323);
-})
+    test('make 123 + 200 return 6', () => {
+        expect(calculator.add(123, 200)).toBe(323);
+    })
 
-test('make 1.23 + 2 return 3.23', () => {
-    expect(calculator.add(1.23, 2)).toBeCloseTo(3.23);
-})
+    test('make 1.23 + 2 return 3.23', () => {
+        expect(calculator.add(1.23, 2)).toBeCloseTo(3.23);
+    })
 
-test('make -5 + -2 return -7', () => {
-    expect(calculator.add(-5, -2)).toBeCloseTo(-7);
-})
-
-
-// Tests for subtract function
-test('calculator object contains subtract function', () => {
-    expect(typeof calculator.subtract).toBe('function');
-})
-
-test('make 5 - 3 return 2', () => {
-    expect(calculator.subtract(5, 3)).toBe(2);
-})
-
-test('make 100 - 35 return 65', () => {
-    expect(calculator.subtract(100, 35)).toBe(65);
-})
-
-test('make 7.3 - 3.1 return 4.2', () => {
-    expect(calculator.subtract(7.3, 3.1)).toBeCloseTo(4.2);
-})
-
-test('make 3 - 24 return -21', () => {
-    expect(calculator.subtract(3, 24)).toBe(-21);
+    test('make -5 + -2 return -7', () => {
+        expect(calculator.add(-5, -2)).toBeCloseTo(-7);
+    })
 })
 
 
-// Tests for divide function
-test('calculator object contains divide function', () => {
-    expect(typeof calculator.divide).toBe('function');
+describe('subtract function', () => {
+    test('calculator object contains subtract function', () => {
+        expect(typeof calculator.subtract).toBe('function');
+    })
+
+    test('make 5 - 3 return 2', () => {
+        expect(calculator.subtract(5, 3)).toBe(2);
+    })
+
+    test('make 100 - 35 return 65', () => {
+        expect(calculator.subtract(100, 35)).toBe(65);
+    })
+
+    test('make 7.3 - 3.1 return 4.2', () => {
+        expect(calculator.subtract(7.3, 3.1)).toBeCloseTo(4.2);
+    })
+
+    test('make 3 - 24 return -21', () => {
+        expect(calculator.subtract(3, 24)).toBe(-21);
+    })
 })
 
-test('make 12 / 4 return 3', () => {
-    expect(calculator.divide(12, 4)).toBe(3);
+
+describe('divide function', () => {
+    test('calculator object contains divide function', () => {
+        expect(typeof calculator.divide).toBe('function');
+    })
+
+    test('make 12 / 4 return 3', () => {
+        expect(calculator.divide(12, 4)).toBe(3);
+    })
+
+    test('make 12 / 4 return 3', () => {
+        expect(calculator.divide(100, 25)).toBe(4);
+    })
+
+    test('make 27.2 / 3.2 return 8.5', () => {
+        expect(calculator.divide(27.2, 3.2)).toBeCloseTo(8.5);
+    })
+
+    test('make -32 / -4 return 8', () => {
+        expect(calculator.divide(-32, -4)).toBeCloseTo(8);
+    })
 })
 
-test('make 12 / 4 return 3', () => {
-    expect(calculator.divide(100, 25)).toBe(4);
-})
 
-test('make 27.2 / 3.2 return 8.5', () => {
-    expect(calculator.divide(27.2, 3.2)).toBeCloseTo(8.5);
-})
+describe('multiply function', () => {
+    test('calculator object contains multiply function', () => {
+        expect(typeof calculator.multiply).toBe('function');
+    })
 
-test('make -32 / -4 return 8', () => {
-    expect(calculator.divide(-32, -4)).toBeCloseTo(8);
-})
+    test('make 5 * 3 return 15', () => {
+        expect(calculator.multiply(5, 3)).toBe(15);
+    })
 
+    test('make 22 * 31 return 682', () => {
+        expect(calculator.multiply(22, 31)).toBe(682);
+    })
 
-// Multiply function tests
-test('calculator object contains multiply function', () => {
-    expect(typeof calculator.multiply).toBe('function');
-})
+    test('make 1.2 * 9.4 return 682', () => {
+        expect(calculator.multiply(1.2, 9.4)).toBeCloseTo(11.28);
+    })
 
-test('make 5 * 3 return 15', () => {
-    expect(calculator.multiply(5, 3)).toBe(15);
-})
-
-test('make 22 * 31 return 682', () => {
-    expect(calculator.multiply(22, 31)).toBe(682);
-})
-
-test('make 1.2 * 9.4 return 682', () => {
-    expect(calculator.multiply(1.2, 9.4)).toBeCloseTo(11.28);
-})
-
-test('make -4.2 * -0.5 return 682', () => {
-    expect(calculator.multiply(-4.2, -0.5)).toBeCloseTo(2.1);
+    test('make -4.2 * -0.5 return 682', () => {
+        expect(calculator.multiply(-4.2, -0.5)).toBeCloseTo(2.1);
+    })
 })
