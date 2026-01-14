abd5a363c9c9a8e6e887d83536f91f9fa4850b16
Shalama <shalamafadillaa@gmail.com>
2026-01-14 13:42:41 +0700

    Make analyzeArray function returns correct form
    Return an object with properties with correct values.

--- src/analyzeArray.spec.js
+++ src/analyzeArray.spec.js
@@ -6,22 +6,33 @@ test('analyzeArray function is defined', () => {
 
 test('make analyzeArray returns an object with properties', () => {
     expect(analyzeArray([1,8,3,4,2,6]))
-    .toBe(
-        Object == { 
+    .toMatchObject(
+        { 
                     average: 4,
                     min: 1,              
                     max: 8,
                     length: 6 
-                  })
+        })
 })
 
 test('make analyzeArray returns an object with properties', () => {
     expect(analyzeArray([3, 5, 7, 8, 9]))
-    .toBe(
-        Object == { 
+    .toMatchObject(
+        { 
                     average: 6.4,
                     min: 3,              
                     max: 9,
                     length: 5 
-                  })
+        })
+})
+
+test('make analyzeArray returns an object with properties', () => {
+    expect(analyzeArray([12.4, 7.2, 13.5, 10.2]))
+    .toMatchObject(
+        { 
+                    average: 10.825,
+                    min: 7.2,              
+                    max: 13.5,
+                    length: 4 
+        })
 })
