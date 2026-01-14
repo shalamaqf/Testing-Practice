c19dd2e231b0e5169f0976fb8fe11b950c33de2f
Shalama <shalamafadillaa@gmail.com>
2026-01-14 11:29:50 +0700

    Make analyzeArray function returns an object
    Make the function returns an object with following properties (average, min, max, length).

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,1 +1,8 @@
-export function analyzeArray() {};
+export function analyzeArray(arrayOfNumbers) {
+    return Object == {
+                        average: 4,
+                        min: 1,
+                        max: 8,
+                        length: 6
+                    }
+};
--- src/analyzeArray.spec.js
+++ src/analyzeArray.spec.js
@@ -3,3 +3,14 @@ import { analyzeArray } from "./analyzeArray.js";
 test('analyzeArray function is defined', () => {
     expect(analyzeArray).toBeDefined();
 })
+
+test('make analyzeArray returns an object with properties', () => {
+    expect(analyzeArray([1,8,3,4,2,6]))
+    .toBe(
+        Object == { 
+                    average: 4,
+                    min: 1,              
+                    max: 8,
+                    length: 6 
+                  })
+})
